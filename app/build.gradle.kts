6179fb0c7f5d49d128e3947e734568f3e8459bbc
Hesam <darvishiyan@gmail.com>
2019-02-28 02:28:01 +0330

    update to kotlin DSL

---
+++ app/build.gradle.kts
@@ -0,0 +1,37 @@
+import org.jetbrains.kotlin.config.KotlinCompilerVersion
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+}
+
+android {
+    compileSdkVersion(28)
+    defaultConfig {
+        minSdkVersion(16)
+        targetSdkVersion(28)
+        applicationId = "com.darvishiyan.gradlekotlindsl"
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(kotlin("stdlib-jdk7", KotlinCompilerVersion.VERSION))
+
+    implementation("androidx.appcompat:appcompat:1.0.0-beta01")
+    implementation("androidx.core:core-ktx:1.1.0-alpha04")
+
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("androidx.test:runner:1.1.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.1.1")
+}
